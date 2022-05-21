589403722c3a63b4f5ca31254ce61e320209b22b
Five-great <43601963+Five-great@users.noreply.github.com>
2022-05-21 07:45:55 +0000

    Update skeleton.md

--- assets/chunk/tpImportword_t_mdufoj1653101234269_p_.js
+++
@@ -1,2 +0,0 @@
-import"./pinia.js";import{J as o}from"./vue.js";const d=o({components:{},template:`<div class="fv-mardown-html"><div class="fv-mardown-main"><h1 id="tpImportword" tabindex="-1"><a class="header-anchor" href="#tpImportword">#</a> tpImportword</h1>
-</div></div><PagesRouter  docPath="__docs__/tpImportword.md" mapType="docs" docRepo="tp-importword" pagesName="tpImportword_t_mdufoj1653101234269_p_" />`});export{d as default};
--- assets/chunk/tpIndent2em_t_ejcv6i1653109950482_p_.js
+++
@@ -1,2 +0,0 @@
-import"./pinia.js";import{J as e}from"./vue.js";const p=e({components:{},template:`<div class="fv-mardown-html"><div class="fv-mardown-main"><p>#test</p>
-</div></div><PagesRouter  docPath="__docs__/tpIndent2em.md" mapType="docs" docRepo="tp-indent2em" pagesName="tpIndent2em_t_ejcv6i1653109950482_p_" />`});export{p as default};
