3ca9728c0b3ba1549b1c8b568a89600e5e7c5ccc
Five-great <43601963+Five-great@users.noreply.github.com>
2022-05-21 05:28:06 +0000

    Update skeleton.md

---
+++ assets/chunk/tpIndent2em_t_u8olhl1653110784873_p_.js
@@ -0,0 +1,2 @@
+import"./pinia.js";import{J as t}from"./vue.js";const p=t({components:{},template:`<div class="fv-mardown-html"><div class="fv-mardown-main"><p>#test</p>
+</div></div><PagesRouter  docPath="__docs__/tpIndent2em.md" mapType="docs" docRepo="tp-indent2em" pagesName="tpIndent2em_t_u8olhl1653110784873_p_" />`});export{p as default};
