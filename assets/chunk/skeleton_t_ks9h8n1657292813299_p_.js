ffa1bf2eb4ff382a2147953b5512415e810411b3
Five-great <43601963+Five-great@users.noreply.github.com>
2022-07-08 15:09:06 +0000

    Update quickStart.md form @ tinymce-plugin/tp-importword@da33abab5a151d2529d63236e47a9f68490754e6 🚀

---
+++ assets/chunk/skeleton_t_ks9h8n1657292813299_p_.js
@@ -0,0 +1,1 @@
+import"./pinia.js";import{M as e}from"./vue.js";const s=e({components:{},template:'<div class="fv-mardown-html"><div class="fv-mardown-main tp-doc"></div></div><PagesRouter  docPath="en/guide/skill/skeleton.md" mapType="docs" docRepo="tinymce-plugin-docs" pagesName="skeleton_t_ks9h8n1657292813299_p_" />'});export{s as default};
